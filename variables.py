8b925e54284d2d7c8e6c2f6f1b26dacb98aebf98
louis.medo <louis@loutik.fr>
2026-01-18 20:32:52 +0100

    Ajout de la veille CVE

--- variables.py
+++ variables.py
@@ -10,3 +10,7 @@ DEVELOPPEURS = ("Ewen GADONNAUD", "Louis MEDO", "Amine KADA")
 # ID du salon de veille
 VEILLE_CHANNEL_ID = os.getenv("VEILLE_CHANNEL_ID")
 print(f"VEILLE_CHANNEL_ID chargé : {VEILLE_CHANNEL_ID}")
+
+# ID du salon pour les CVEs
+VEILLE_CVE_CHANNEL_ID = os.getenv("VEILLE_CVE_CHANNEL_ID")
+print(f"VEILLE_CHANNEL_ID chargé : {VEILLE_CVE_CHANNEL_ID}")
